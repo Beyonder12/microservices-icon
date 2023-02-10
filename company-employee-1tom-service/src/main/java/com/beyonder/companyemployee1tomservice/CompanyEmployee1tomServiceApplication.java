04aa568546a7c40108af09087f0669db85e2b418
Beyonder12 <fajri.illahi1211@gmail.com>
2023-02-10 17:42:43 +0700

    init company-employee-1tom-service

---
+++ company-employee-1tom-service/src/main/java/com/beyonder/companyemployee1tomservice/CompanyEmployee1tomServiceApplication.java
@@ -0,0 +1,13 @@
+package com.beyonder.companyemployee1tomservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyEmployee1tomServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CompanyEmployee1tomServiceApplication.class, args);
+    }
+
+}
---
+++ company-employee-1tom-service/src/test/java/com/beyonder/companyemployee1tomservice/CompanyEmployee1tomServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.beyonder.companyemployee1tomservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyEmployee1tomServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

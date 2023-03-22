fa197dbc6d91cbad6a6d5184bf4848be97ae857d
Beyonder12 <fajri.illahi1211@gmail.com>
2023-03-22 20:47:13 +0700

    add config for cors frontend

--- gateway-service/src/main/java/com/beyonder/gatewayservice/config/AppConfig.java
+++ gateway-service/src/main/java/com/beyonder/gatewayservice/config/AppConfig.java
@@ -3,6 +3,9 @@ package com.beyonder.gatewayservice.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class AppConfig {
@@ -10,4 +13,20 @@ public class AppConfig {
     public RestTemplate restTemplate() {
         return new RestTemplate();
     }
+
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedHeader("Requestor-Type");
+        configuration.addExposedHeader("X-Get-Header");
+        configuration.addAllowedOrigin("*");
+        configuration.addAllowedMethod("*");
+        configuration.addExposedHeader("Access-Control-Allow-Origin");
+        configuration.addExposedHeader("Access-Control-Allow-Credentials");
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
 }
